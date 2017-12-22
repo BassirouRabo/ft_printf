87523d99c05815808da0408922eb8f5ca7372b4d
Bassirou Rabo Hima <brabo-hi@student.42.us.org>
2017-12-21 22:26:42 -0800

    clean 2

--- ft_set2.c
+++ ft_set2.c
@@ -6,7 +6,7 @@
 /*   By: brabo-hi <brabo-hi@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 01:06:54 by brabo-hi          #+#    #+#             */
-/*   Updated: 2017/12/21 22:02:16 by brabo-hi         ###   ########.fr       */
+/*   Updated: 2017/12/21 22:25:50 by brabo-hi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,17 @@ char	*ft_set_x(va_list *args, t_cs *cs)
 	min = ft_atoi(out) ? min : 1;
 	size_prefix = cs->flag1 ? 2 : 0;
 	size_prefix = ft_atoi(out) ? size_prefix : 0;
-
 	dest = ft_add_str(out, ft_find_max(cs, min, size_prefix), cs);
-
-	if (!cs->precision && cs->digit  && !ft_atoi(dest) && min-- && !(dest = ft_delete_last_zero(cs, dest)))
-		return (NULL);
-
-	if (cs->precision > ft_strlen(out) + size_prefix
-			&& !(dest = ft_add_zero(dest, (cs->precision + size_prefix), size_prefix)))
-		return (NULL);
-
-	if (!cs->precision && !cs->flag3 && cs->flag2
-			&& !(dest = ft_add_zero(dest, ft_strlen(dest), size_prefix)))
-		return (NULL);
-
-	if (size_prefix && !(dest = ft_add_prefix(dest, '0', 'x')))
-		return (NULL);
-
-	if (cs->flag3 && !(dest = ft_to_left(dest)))
-		return (NULL);
-	
+	if (!cs->precision && cs->digit  && !ft_atoi(dest) && min--)
+	   dest = ft_delete_last_zero(cs, dest);
+	if (cs->precision > ft_strlen(out) + size_prefix)
+		dest = ft_add_zero(dest, (cs->precision + size_prefix), size_prefix);
+	if (!cs->precision && !cs->flag3 && cs->flag2)
+		dest = ft_add_zero(dest, ft_strlen(dest), size_prefix);
+	if (size_prefix)
+	   dest = ft_add_prefix(dest, '0', 'x');
+	if (cs->flag3)
+		dest = ft_to_left(dest);
 	if (cs->type == 'X')
 		while (++i < ft_strlen(dest))
 			dest[i] = (char)ft_toupper(dest[i]);
@@ -63,20 +54,16 @@ char	*ft_set_o(va_list *args, t_cs *cs)
 	min = cs->flag1 ? ft_strlen(dest) + 1 : ft_strlen(dest);
 	min = cs->precision ? cs->precision : min;
 	dest = ft_add_str(dest, ft_find_max(cs, min, 0), cs);
-	if (!cs->precision && cs->digit  && !ft_atoi(dest) && min-- && !(dest = ft_delete_last_zero(cs, dest)))
-		return (NULL);
-	if ((cs->precision || cs->flag2) &&
-			!(dest = ft_add_zero(dest, min, cs->flag1 ? 1 : 0)))
-		return (NULL);
-
-	if (!cs->precision && !cs->flag3 && cs->flag2
-			&& !(dest = ft_add_zero(dest, ft_strlen(dest), cs->flag1 ? 1 : 0)))
-		return (NULL);
-
-	if (cs->flag1 && !(dest = ft_add_prefix(dest, '0', 0)))
-		return (NULL);
-	if (cs->flag3 && !(dest = ft_to_left(dest)))
-		return (NULL);
+	if (!cs->precision && cs->digit  && !ft_atoi(dest) && min--)
+	   dest = ft_delete_last_zero(cs, dest);
+	if ((cs->precision || cs->flag2))
+		dest = ft_add_zero(dest, min, cs->flag1 ? 1 : 0);
+	if (!cs->precision && !cs->flag3 && cs->flag2)
+		dest = ft_add_zero(dest, ft_strlen(dest), cs->flag1 ? 1 : 0);
+	if (cs->flag1)
+	   dest = ft_add_prefix(dest, '0', 0);
+	if (cs->flag3)
+	   dest = ft_to_left(dest);
 	return (dest);
 }
 
@@ -90,8 +77,8 @@ char	*ft_set_p(va_list *args, t_cs *cs)
 	max = cs->width > ft_strlen(dest) ? cs->width : ft_strlen(dest);
 	dest = ft_add_str(dest, max, cs);
 	dest = ft_add_prefix(dest, '0', 'x');
-	if (cs->flag3 && !(dest = ft_to_left(dest)))
-		return (NULL);
+	if (cs->flag3)
+		dest = ft_to_left(dest);
 	return (dest);
 }
 
@@ -100,13 +87,13 @@ char	*ft_set_z(va_list *args, t_cs *cs)
 	char	*dest;
 	int		max;
 
-	if (!(dest = ft_memalloc(sizeof(char) * 2)))
+	if (!(dest = ft_memalloc(2)))
 		exit(0);
 	dest[0] = '%';
 	dest[1] = '\0';
 	max = cs->width ? cs->width : 1;
 	dest = ft_add_str(dest, max, cs);
-	if (cs->flag3 && !(dest = ft_to_left(dest)))
-		return (NULL);
+	if (cs->flag3)
+	   dest = ft_to_left(dest);
 	return (dest);
 }
